634c4dd2cebdb1b23f1e07e73c5803ae2200e205
gerazaxel <dimagerazax@gmail.com>
2025-04-25 23:02:35 +0300

    hw1

---
+++ lab1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>241-321 Казаков Дмитрий lab1</title>
+</head>
+<body>
+    <header style="display: flex; justify-content: left; gap: 5%;">
+        <img src="./logo.jpg" alt="Логотип" style="width: 30%; height: 20%;">
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+    <main>
+        <p><?php echo "Hello, world!" ?></p>
+        <p>
+            <?php echo date("l");?>
+        </p>
+    </main>
+    <footer>
+        <p>Задание для самостоятельной работы (без описания)</p>
+    </footer>
+</body>
+</html>
